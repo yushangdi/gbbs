10728abd1d857f77496133f5e6ce4e7d161c1d1a
shangdiy <shangdiy@mit.edu>
2020-02-24 18:14:35 -0500

    progress

---
+++ benchmarks/TriangleCounting/LiuShun/Triangle.h
@@ -0,0 +1,157 @@
+// This code is part of the project "Theoretically Efficient Parallel Graph
+// Algorithms Can Be Fast and Scalable", presented at Symposium on Parallelism
+// in Algorithms and Architectures, 2018.
+// Copyright (c) 2018 Laxman Dhulipala, Guy Blelloch, and Julian Shun
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all  copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include "pbbslib/sample_sort.h"
+#include "pbbslib/monoid.h"
+#include "ligra/pbbslib/sparse_table.h"
+#include "ligra/ligra.h"
+
+#include "benchmarks/DegeneracyOrder/BarenboimElkin08/DegeneracyOrder.h"
+#include "benchmarks/DegeneracyOrder/GoodrichPszona11/DegeneracyOrder.h"
+#include "benchmarks/KCore/JulienneDBS17/KCore.h"
+
+using namespace std;
+
+#define OLD_EDGE 0
+#define NEW_INSERTION 1
+#define NEW_DELETION 2
+
+struct hash_pair {
+  inline size_t operator () (const std::tuple<uintE, uintE>& t) {
+    size_t l = std::min(std::get<0>(t), std::get<1>(t));
+    size_t r = std::max(std::get<0>(t), std::get<1>(t));
+    size_t key = (l << 32) + r;
+    return pbbslib::hash64_2(key);
+  }
+};
+
+
+template <class Graph, class E, class F>
+struct BPDTriangleCountState {
+  Graph& G;
+  size_t M;
+  size_t t1;
+  size_t t2;
+  pbbs::sequence<uintE> D;
+  E& HH; 
+  E& HL; 
+  E& LH; 
+  E& LL; 
+  F& T;
+
+  BPDTriangleCountState(Graph& _G, E _HH, E _HL, E _LH, E _LL, F _T): 
+    G(_G),
+    // D(_D),
+    HH(_HH),
+    HL(_HL),
+    LH(_LH),
+    LL(_LL),
+    T(_T){
+
+    size_t n = G.n;
+    M  = 2 * G.m + 1;
+    t1 = sqrt(M) / 2;
+    t2 = 3 * t1;
+
+
+    // auto init_D_f = [&](const uintE& v) {
+    //   D[v] = G.get_vertex(v).getOutDegree();
+    // };
+    auto frontier = pbbs::sequence<bool>(n, true);
+    // vertexMap(activeAndCts, init_D_f);
+
+    D = pbbs::sequence<uintE>(n, [&] (size_t i) { return G.get_vertex(i).getOutDegree(); });
+    edgeMap(G, )
+  }
+
+
+  ~BPDTriangleCountState(){
+    // free(D); ??
+    // HH.del();
+    // HL.del();
+    // LH.del();
+    // LL.del();
+    // T.del();
+  }
+
+};
+
+
+
+template <class Graph, class E, class F>
+struct updateTablesF {
+  Graph& G;
+  BPDTriangleCountState<Graph, E, F>& state;
+  updateTablesF(Graph& G, BPDTriangleCountState<Graph, E, F>& _state) : G(G), state(_state) {}
+
+  inline bool update(uintE s, uintE d) {// (1,2) (2,1) will be called twice?
+    state.D[s] += 1;
+    // auto d_vertex = G.get_vertex(d);
+    // pbbslib::write_add(&counts[s], G.get_vertex(s).intersect(&d_vertex, s, d));
+    return 1;
+  }// when to return false?
+
+  inline bool updateAtomic(uintE s, uintE d) {
+    // auto d_vertex = G.get_vertex(d);
+    // pbbslib::write_add(&counts[s], G.get_vertex(s).intersect(&d_vertex, s, d));
+    return 1;
+  }
+  inline bool cond(uintE d) { return cond_true(d); }
+};
+
+template <class Graph>
+inline auto Initialize(Graph& G){
+  using K = std::pair<uintE, uintE>;
+  using V = int8_t;
+  using KV = std::tuple<K, V>;
+  // using W = typename Graph::weight_type;
+  size_t n = G.n;
+  KV empty = std::make_tuple(std::make_pair(UINT_E_MAX, UINT_E_MAX), -1);
+
+  auto HH = sparse_table<K, V, hash_pair>(n, empty, hash_pair());
+  auto HL = sparse_table<K, V, hash_pair>(n, empty, hash_pair());
+  auto LH = sparse_table<K, V, hash_pair>(n, empty, hash_pair());
+  auto LL = sparse_table<K, V, hash_pair>(n, empty, hash_pair());
+  auto T  = sparse_table<K, size_t, hash_pair>(n, empty, hash_pair());
+  // auto D  = sequence<size_t>(n);
+
+  using E = decltype(HH);
+  using F = decltype(T);
+  
+  auto state = BPDTriangleCountState<Graph, E, F>(G, HH, HL, LH, LL, T);
+
+  return state;
+}
+
+
+template <class Graph, class F>
+inline size_t Triangle(Graph& G, const F& f, commandLine& P) {
+  auto C0 = P.getOptionIntValue("-c", 0);
+
+  auto state = Initialize<Graph>(G);
+
+  return C0;
+}
---
+++ ysd_run.sh
@@ -0,0 +1,1 @@
+bazel run //benchmarks/TriangleCounting/LiuShun:Triangle_main -- -s ~/gbbs/inputs/rMatGraph_J_5_100
